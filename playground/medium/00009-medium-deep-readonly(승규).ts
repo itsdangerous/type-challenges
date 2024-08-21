fc7ebe765d78e447b39b9aa2585b5d418c48ee32
itsdangerous <danger121@naver.com>
2024-08-21 23:35:52 +0900

    solve: deep-readonly

--- playground/medium/00009-medium-deep-readonly(승규).ts
+++ playground/medium/00009-medium-deep-readonly(승규).ts
@@ -36,15 +36,60 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type DeepReadonly<T> = any
+
+
+// try 1 -
+// 재귀로 들어가지 않고 depth가 1인 객체의 키만 readonly로 변환
+// type DeepReadonly<T> = {
+//   readonly [K in keyof T]: T[K];
+// }
+
+// tyr 2 -
+// 재귀로 들어가지만 함수타입은 그대로 내보내야 함
+// type DeepReadonly<T> = {
+//   readonly [K in keyof T]: DeepReadonly<T[K]>;
+// };
+
+// try 3- 성공
+type DeepReadonly<T> = {
+  readonly [K in keyof T]: T[K] extends Function ? T[K] : DeepReadonly<T[K]>;
+};
+
+
+// 이전에 재경 선생님이 알려주신 방식으로 시도해봄
+// type DeepReadonly<T> = {
+//   readonly [K in keyof T]: T[K] extends (...args: any[]) => any ? T[K] : DeepReadonly<T[K]>;
+// };
+
+// try 4 - Function 대신, 내장 유틸로만 구현
+// type IsFunction<T> = T extends (...args: any[]) => any ? true : false;
+
+// type DeepReadonly<T> = {
+//   readonly [K in keyof T]: IsFunction<T[K]> extends true ? T[K] : DeepReadonly<T[K]>;
+// };
+
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
-
 type cases = [
+  Expect<Equal<DeepReadonly<X00>, Expected00>>,
+  Expect<Equal<DeepReadonly<X0>, Expected0>>,
   Expect<Equal<DeepReadonly<X1>, Expected1>>,
   Expect<Equal<DeepReadonly<X2>, Expected2>>,
 ]
+
+type X00 = {
+   a:() => 22
+   b: string
+   c: {
+     d: boolean
+  }
+}
+
+type X0 = {
+  a: () =>22
+  b:string
+}
 
 type X1 = {
   a: () => 22
@@ -71,6 +116,18 @@ type X1 = {
 
 type X2 = { a: string } | { b: number }
 
+type Expected00 = {
+  readonly a:() => 22
+  readonly b: string
+  readonly c: {
+    readonly d: boolean
+  }
+}
+
+type Expected0 = {
+  readonly a: () => 22
+  readonly b: string
+}
 type Expected1 = {
   readonly a: () => 22
   readonly b: string
@@ -96,9 +153,104 @@ type Expected1 = {
 
 type Expected2 = { readonly a: string } | { readonly b: number }
 
-/* _____________ 다음 단계 _____________ */
-/*
-  > 정답 공유하기: https://tsch.js.org/9/answer/ko
-  > 정답 보기: https://tsch.js.org/9/solutions
-  > 다른 문제들: https://tsch.js.org/ko
-*/
+//////////////// 추가 //////////////////
+//////////////// 추가 //////////////////
+//////////////// 추가 //////////////////
+//////////////// 추가 //////////////////
+//////////////// 추가 //////////////////
+
+
+// 참고 url
+// https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
+
+// keyof 타입 연산자
+// keyof 연산자는 객체 타입에서 객체의 키 값들을 숫자나 문자열 리터럴 유니온을 생성한다.
+// 아래의 타입 P는 "x" | "y"와 동일한 타입이다.
+
+type Point = { 
+  x: number;
+  y:number;
+}
+type P = keyof Point;
+
+// 만약 타입이 string이나 number 인덱스 시그니쳐를 가지고 있다면 keyof는 해당 타입을 리턴한다.
+type AA = { [n: number]: unknown};
+type A = keyof AA; // type A = number
+
+type AA2 = { [n: string]: unknown};
+type A2 = keyof AA2; // type 2 = string | number
+
+// 여기서, string 시그니쳐를 가진다마면 string | number를 리턴하게되는데,
+// 'javascript 객체 키는 항상 문자열을 강제' 하기 때문에, obj[0]은 obj["0"]과 동일하다.
+
+const 예시1 = {
+  a: '문자', // (property) a : string
+  0: '숫자', // (property) 0 : string
+  true: '부울', // (property) true: string
+  99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999 : ';',
+}
+
+// 만약 미리 선언되지 않은 속성유형 즉, 하드코딩으로 일일이 하나하나 시그니처를 부여하지 않는 방법으로는 index signature 방법을 사용할 수 있다.
+
+type Horse ={
+  ehihihing: string;
+}
+
+// 하드코딩
+type OnlyBoolsAndHorses = {
+  del: boolean,
+  rodney: boolean,
+  anyKey: {
+    ehihihing: string,
+  }
+}
+
+// index signature
+// type OnlyBoolsAndHorses = {
+//   [key: string]: boolean | Horse;
+// }
+
+const conforms: OnlyBoolsAndHorses = {
+  del: true,
+  rodney: false,
+  anyKey: { ehihihing: "some value" },
+}
+
+// 그렇다면 mapped types를 정의하기 위해서는?
+// keyof는 객체 타입의 키들의 유니온 타입을 반환한다고 위에서 공부한 바 있다.
+// keyof 키워드 앞에 'in'을 붙여주게 된다면, 이 유니온 타입을 순회한다.
+
+// 예시를 보자
+type Person = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+}
+
+type ReadonlyPerson = {
+  readonly [K in keyof Person]: Person[K];
+}
+
+// type ReadonlyPerson = {
+//   readonly name: string;
+//   readonly age: number;
+//   readonly isStudent: boolean;
+// }
+
+// 우리는 이전 학습을 통해, 타입을 제한하기 위해 extends 키워드를 공부했다.
+// 그렇다면 in keyof와 extends는 무엇이 다른가 고민해보았다.
+
+type ReadonlyPerson = {
+  readonly [K extends Person]: Person[K];
+} 
+
+// 위 타입 선언은 에러가 난다.
+// extends 키워드는 타입을 '제약'하는 것이기 때문이다.
+// extends는 타입 제약을 나타내기 위해 사용되며, '제네릭 타입 매개변수'를 제한하거나 '조건부 타입'을 정의할 때 사용된다.
+// 예시로, T extends U 는 'T'가 'U'의 서브타입이어야 한다는 의미다.
+
+// in keyof는 mapped type을 정의할때 사용된다.
+// keyof는 객체 타입의 모든 키들의 유니언 타입을 생성하며, in 키워드는 이 유니언 타입의 각 키를 순회하고, 이 키워드가 대괄호 안에 있다면 새로운 타입을 동적으로 생성할 수 있게 해준다.
+// 예시로, [K in keyof T]는 타입 'T'의 모든 키를 순회하며 매핑된 타입을 '생성'한다.
+
+///////여기서 주의해야할 점은 '제한'과 '생성'이다./////////
